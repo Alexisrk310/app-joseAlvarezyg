1a4ab10c8f6de270fbda9d8ccd8aae064fcd3646
alexis <alexisrk310@gmail.com>
2022-12-22 21:31:57 -0500

    bar responsive

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,65 +4,76 @@ import './styles/NavBar.css';
 export interface NavBarInterface {}
 
 const NavBar: React.FC<NavBarInterface> = () => {
+	const handleToggle = () => {
+		const bar = document.querySelector('.navbar');
+		const toggle = document.querySelector('.container-navbar');
+		toggle?.classList.toggle('responsive');
+		bar?.classList.toggle('bar-toggle');
+	};
 	return (
-		<div className="navbar sticky-top">
-			<NavLink
-				to={'/'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				HOME
-			</NavLink>
-			<NavLink
-				to={'restaurante'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				RESTAURANTES
-			</NavLink>
-			{/* <NavLink
+		<div className="container-navbar sticky-top">
+			<div className="navbar">
+				<NavLink
+					to={'/'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					HOME
+				</NavLink>
+				<NavLink
+					to={'restaurante'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					RESTAURANTES
+				</NavLink>
+				{/* <NavLink
 				to={'ven'}
 				className={({ isActive }) =>
 					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
 				}>
 				VEN TE ENSEÑO
-			</NavLink> */}
+				</NavLink> */}
 
-			<NavLink
-				to={'tour'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				TOUR GASTRONOMICO
-			</NavLink>
-			<NavLink
-				to={'videos'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				VIDEOS PUBLICADOS
-			</NavLink>
-			<NavLink
-				to={'team'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				TEAM JOSE ALVAREZ
-			</NavLink>
-			<NavLink
-				to={'contactanos'}
-				className={({ isActive }) =>
-					isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
-				}>
-				CONTACTANOS
-			</NavLink>
-			<i className="fa-solid fa-magnifying-glass pointer white"></i>
-			<NavLink to={'login'} className="pointer btn btn-info">
-				Inicia Sesión
-			</NavLink>
-			<NavLink to={'register'} className="pointer btn btn-info">
-				Registrate
-			</NavLink>
+				<NavLink
+					to={'tour'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					TOUR GASTRONOMICO
+				</NavLink>
+				<NavLink
+					to={'videos'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					VIDEOS PUBLICADOS
+				</NavLink>
+				<NavLink
+					to={'team'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					TEAM JOSE ALVAREZ
+				</NavLink>
+				<NavLink
+					to={'contactanos'}
+					className={({ isActive }) =>
+						isActive ? 'isActive pointer navbar-brand' : 'pointer navbar-brand'
+					}>
+					CONTACTANOS
+				</NavLink>
+				<i className="fa-solid fa-magnifying-glass pointer white"></i>
+				<NavLink to={'login'} className="pointer btn btn-info">
+					Inicia Sesión
+				</NavLink>
+				<NavLink to={'register'} className="pointer btn btn-info">
+					Registrate
+				</NavLink>
+			</div>
+			<i
+				className="fa-solid fa-bars barJose pointer"
+				onClick={handleToggle}></i>
 		</div>
 	);
 };
--- src/utilities/api/auth/auth.ts
+++ src/utilities/api/auth/auth.ts
@@ -4,7 +4,6 @@ export const auth = async (endpoint: string, inputsValues: any) => {
 	const urlRegister = `https://backen-appjose.vercel.app/auth/${endpoint}`;
 	return await fetch(urlRegister, {
 		method: 'POST',
-
 		headers: {
 			'Content-type': 'application/json',
 		},
